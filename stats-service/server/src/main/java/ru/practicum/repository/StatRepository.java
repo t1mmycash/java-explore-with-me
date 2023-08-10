a01d138e07ed3afd362098878b9d889aa6d0caa8
t1mmycash <timaknizh@gmail.com>
2023-08-10 16:48:05 +0300

    feat: надеюсь сработает

--- stats-service/server/src/main/java/ru/practicum/repository/StatRepository.java
+++ stats-service/server/src/main/java/ru/practicum/repository/StatRepository.java
@@ -12,26 +12,26 @@ import java.util.List;
 
 @Repository
 public interface StatRepository extends JpaRepository<Stat, Integer> {
-    @Query("select new ru.practicum.dto.ViewStats(s.app, s.uri, count(s.ip)) " +
+    @Query("select new ru.practicum.dto.ViewStats(s.app, s.uri, count(s.ip) as hits) " +
             "from Stat as s " +
             "where s.timestamp between ?1 and ?2 " +
             "group by s.app, s.uri ")
     List<ViewStats> getStats(LocalDateTime start, LocalDateTime end, Sort sort);
 
-    @Query("select new ru.practicum.dto.ViewStats(s.app, s.uri, count(distinct s.ip)) " +
+    @Query("select new ru.practicum.dto.ViewStats(s.app, s.uri, count(distinct s.ip) as hits) " +
             "from Stat as s " +
             "where s.timestamp between ?1 and ?2 " +
             "group by s.app, s.uri ")
     List<ViewStats> getUniqueIpStats(LocalDateTime start, LocalDateTime end, Sort sort);
 
-    @Query("select new ru.practicum.dto.ViewStats(s.app, s.uri, count(s.ip)) " +
+    @Query("select new ru.practicum.dto.ViewStats(s.app, s.uri, count(s.ip) as hits) " +
             "from Stat as s " +
             "where s.timestamp between ?1 and ?2 " +
             "and s.uri in ?3 " +
             "group by s.app, s.uri ")
     List<ViewStats> getStatsByUris(LocalDateTime start, LocalDateTime end, List<String> uris, Sort sort);
 
-    @Query("select new ru.practicum.dto.ViewStats(s.app, s.uri, count(distinct s.ip)) " +
+    @Query("select new ru.practicum.dto.ViewStats(s.app, s.uri, count(distinct s.ip) as hits) " +
             "from Stat as s " +
             "where s.timestamp between ?1 and ?2 " +
             "and s.uri in ?3 " +
